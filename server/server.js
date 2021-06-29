c316b6ba054bf6e02f6927da7b5cc31c249b514d
latoyadawson <78005063+latoyadawson@users.noreply.github.com>
2021-06-29 16:51:31 -0700

    updated schemas, typeDefs, and resolvers

--- server/server.js
+++ server/server.js
@@ -1,8 +1,23 @@
 const express = require('express');
+//import ApolloServer
+const { ApolloServer } = require('apollo-server-express');
+
+//imprt typeDefs and resolvers 
+const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
+
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+//create a new apollo server and pass in our schema data 
+const server = new ApolloServer({ 
+  typeDefs,
+  resolvers
+});
+
+//integrate Apollo server with the Express application as middleware
+server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -10,5 +25,7 @@ app.use(express.json());
 db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
+    //log where we can go test our GQL API
+    console.log(`Use GraphWL at http://localhost:${PORT}${server.graphqlPath}`)
   });
 });
